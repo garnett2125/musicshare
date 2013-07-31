01c9d3cb2d1309f2e667ff70e62b0e23a64efffc
Yohan <yohann@commerceguys.com>
2013-07-31 16:42:35 +0200

    Follow angular way

--- js/tv-premieres-app/controllers/youtubeController.js
+++ js/tv-premieres-app/controllers/youtubeController.js
@@ -1,45 +1,17 @@
 app.controller("youtubeController", function($scope, $http){
-
+    $scope.youtubePlaylists = [];
     $scope.init = function() {
-      // YOUTUBE.
-      $http({method: 'GET', url: 'https://gdata.youtube.com/feeds/api/users/garnett2125/playlists'}).
-        success(function(data, status, headers, config) {
-          var x2js = new X2JS();
-          var json = x2js.xml_str2json( data );
-          var playlists = json.feed.entry;
-          for (var playlist in playlists) {
-            var playlist = playlists[playlist];
-            var h1 = document.createElement('h1');
-            document.body.appendChild(h1);
-            h1.innerHTML = playlist.title.__text;
-            var ul = document.createElement('ul');
-            $http({method: 'GET', url: playlist.feedLink._href + '?v=2'}).
-              success(function(data, status, headers, config) {
-                var x2js = new X2JS();
-                var json = x2js.xml_str2json( data );
-                var videos = json.feed.entry;
-                for (var video in videos) {
-                  var video  = videos[video];
-                  var li = document.createElement("li");
-                  ul.appendChild(li);
-                  li.innerHTML = video.title;
-                }
-              }).
-              error(function(data, status, headers, config) {
-                alert('error in get request');
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-              });
-            document.body.appendChild(ul);
-          }
-        }).
-        error(function(data, status, headers, config) {
-          alert('error in get request');
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-        });
-
       // DEEZER.
+//      function createPlaylistDeezer() {
+//        // https://api.deezer.com/2.0/user/5027124/playlists
+//        $http({method: 'POST', url: "https://api.deezer.com/2.0/user/5027124/playlists", data: {title : "Test de nouvelle playlist"}}).
+//          success(function(data, status, headers, config) {
+//            alert("Playlist deezer créée avec succès.");
+//          }).
+//          error(function(data, status, headers, config) {
+//            alert("La playlist deezer n'a pas pu etre créée.");
+//          });
+//      }
 //      $http({method: 'GET', url: 'https://api.deezer.com/2.0/user/5027124/playlists'}).
 //        success(function(data, status, headers, config) {
 //          console.log(data);
@@ -53,7 +25,36 @@ app.controller("youtubeController", function($scope, $http){
 //          // called asynchronously if an error occurs
 //          // or server returns response with an error status.
 //        });
-    };
 
+      // YOUTUBE.
+      $http({method: 'GET', url: 'https://gdata.youtube.com/feeds/api/users/garnett2125/playlists'}).
+        success(function(data, status, headers, config) {
+          var x2js = new X2JS();
+          var json = x2js.xml_str2json( data );
+          var playlists = json.feed.entry;
+          angular.forEach(playlists, function(playlist, key){
+            var playlistTitle = playlist.title.__text;
+            // Get content of the playlist.
+            $http({method: 'GET', url: playlist.feedLink._href + '?v=2'}).
+              success(function(data, status, headers, config) {
+                var x2js = new X2JS();
+                var json = x2js.xml_str2json( data );
+                var videos = json.feed.entry;
+                var videoTitles = [];
+                angular.forEach(videos, function(video, key){
+                  videoTitles.push(video.title);
+                });
+                $scope.youtubePlaylists.push({title: playlistTitle, videos: videoTitles});
+                console.log($scope.youtubePlaylists);
+              }).
+              error(function(data, status, headers, config) {
+                alert('error in get request');
+              });
+          });
+        }).
+        error(function(data, status, headers, config) {
+          alert('error in get request');
+        });
+    };
 });
 
