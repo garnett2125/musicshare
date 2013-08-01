50a0f5e4f7844b4822c520f807c18b8d0e6fae69
garnett2125 <yohan.tillier@gmail.com>
2013-08-01 12:30:48 +0200

    Search on soundtrack

--- js/music-share-app/controllers/musicshareController.js
+++ js/music-share-app/controllers/musicshareController.js
@@ -48,29 +48,15 @@ app.controller("musicshareController", function($scope, $http){
           alert('Error when doing a request to Soundcloud to get playlists ');
         });
 
-      // DEEZER.
-      // https://api.deezer.com/2.0/user/5027124/playlists
-//      $http({method: 'POST', url: "https://api.deezer.com/2.0/user/5027124/playlists", data: {title : "Test de nouvelle playlist"}}).
-//        success(function(data, status, headers, config) {
-//          alert("Playlist deezer créée avec succès.");
-//        }).
-//        error(function(data, status, headers, config) {
-//          alert("La playlist deezer n'a pas pu etre créée.");
-//        });
-//      https://connect.deezer.com/oauth/auth.php?app_id=122645&redirect_uri=file://localhost/Users/yohantillier/Documents/Commerceguys/Projects/musicshare/index.html&perms=manage_library
-//      $http({method: 'GET', url: 'https://api.deezer.com/2.0/user/5027124/playlists'}).
-//        success(function(data, status, headers, config) {
-//          console.log(data);
-//          var x2js = new X2JS();
-//          var json = x2js.xml_str2json(data);
-//          console.log(json);
-//        }).
-//        error(function(data, status, headers, config) {
-//          console.log(data);
-//          alert('error in deezer request');
-//          // called asynchronously if an error occurs
-//          // or server returns response with an error status.
-//        });
+      // *********************   CONVERT FROM YOUTUBE TO SOUNDCLOUD.
+      SC.initialize({
+        client_id: 'be88ad87cb242d2e88d6573c08278cb2'
+      });
+
+      // find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
+      SC.get('/tracks', { q: 'Ne-yo Because Of You'}, function(tracks) {
+        console.log(tracks);
+      });
     };
 });
 
