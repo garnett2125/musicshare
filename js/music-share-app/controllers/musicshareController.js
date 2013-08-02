8e7d57c38531ff54bd462c88f5d3212c047cd92b
Mickaël Mao <mickael@commerceguys.com>
2013-08-02 09:09:13 +0200

    Update function to reach youtube tracks for soundcloud.

--- js/music-share-app/controllers/musicshareController.js
+++ js/music-share-app/controllers/musicshareController.js
@@ -5,7 +5,10 @@ app.controller("musicshareController", function($scope, $http){
     $scope.init = function() {
 
       // *********************   YOUTUBE.
-      $http({method: 'GET', url: 'https://gdata.youtube.com/feeds/api/users/garnett2125/playlists'}).
+      var youtubeUser = 'garnett2125';
+      //var youtubeUser = 'ByMikaa';
+
+      $http({method: 'GET', url: 'https://gdata.youtube.com/feeds/api/users/'+ youtubeUser +'/playlists'}).
         success(function(data, status, headers, config) {
           var x2js = new X2JS();
           var json = x2js.xml_str2json( data );
@@ -58,30 +61,6 @@ app.controller("musicshareController", function($scope, $http){
       SC.get('/tracks', { q: 'Ne-yo Because Of You'}, function(tracks) {
         //console.log(tracks);
       });
-
-
-
-
-      // *********************   BUTTONS
-      $scope.searchtracks = [];
-      $scope.search = function(playlists) {
-
-        angular.forEach(playlists, function(value, index) {
-
-          $scope.searchtracks.push({
-            title: playlistTitle,
-            videos: videoTitles
-          });
-        })
-
-        $params = $.param({
-          "name": bookData.name,
-          "price": bookData.price,
-          "author_id": bookData.authorId
-        })
-        //sharedBooks.saveBooks($params);
-      };
-
     };
 });
 
@@ -90,18 +69,15 @@ app.controller("musicshareController", function($scope, $http){
 app.controller('SubmitController', function($scope, $http) {
   $scope.search = function(playlist) {
 
-    //console.log(playlist);
-
+    $scope.playlistTitle = playlist.title;
     angular.forEach(playlist.tracks, function(value, index) {
-
       // Get full title name from playlist.
-
       var yt_url='http://gdata.youtube.com/feeds/api/videos?q='+value.title+'&format=5&max-results=1&v=2&alt=jsonc';
       $http({method: 'GET', url: yt_url, dataType:"jsonp", }).
         success(function(data, status, headers, config) {
 
           console.log(data.data.items[0]);
-          console.log(data.data.items[0].title);
+          $scope.trackTitle = data.data.items[0].title;
         }).
         error(function(data, status, headers, config) {
           alert('Error when doing a request to Youtube to get playlist track');
@@ -125,30 +101,49 @@ app.controller('SubmitController', function($scope, $http) {
     //sharedBooks.saveBooks($params);
 
 
-    console.log(playlist);
+
   };
+
+/*
+  $scope.SoundcloudToYoutube = function(playlist) {
+
+    var yt_url = '';
+    $http({method: 'POST', url: yt_url, dataType:"jsonp", }).
+      success(function(data, status, headers, config) {
+        console.log(data.data.items[0]);
+        $scope.trackTitle = data.data.items[0].title;
+      }).
+      error(function(data, status, headers, config) {
+        alert('Error when doing a request to Youtube to get playlist track');
+      });
+  };*/
+  $scope.trackList = [];
   $scope.searchYoutubeToSoundcloud = function(playlist) {
     var title = playlist.title;
     var videos = playlist.videos;
-    console.log(title);
+    //console.log(title);
     // *********************   CONVERT FROM YOUTUBE TO SOUNDCLOUD.
     SC.initialize({
-      client_id: 'be88ad87cb242d2e88d6573c08278cb2'
+      client_id: 'be88ad87cb242d2e88d6573c08278cb2',
+      redirect_uri: 'http://localhost/musicshare/js/soundcloud/callback.html'
     });
 
     angular.forEach(videos, function(video, key){
       // find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
       SC.get('/tracks', { q: video}, function(tracks) {
-        console.log(tracks[0].id);
+        //console.log(tracks[0].id);
+
+      });
+
+    });
+
+    // Create playlist
+    SC.connect(function() {
+      var tracks = [37600755,21928809].map(function(id) { return { id: id } });
+      SC.post('/playlist', {
+        playlist: { title: 'state', tracks: tracks }
       });
     });
 
-
-    /*$params = $.param({
-      "name": bookData.name,
-      "price": bookData.price,
-      "author_id": bookData.authorId
-    })*/
-    //sharedBooks.saveBooks($params);
   };
 });
